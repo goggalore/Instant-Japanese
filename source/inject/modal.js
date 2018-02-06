1328e7290464094a989a4ce639d7830c2c958e23
goggalore <junec177@gmail.com>
2018-02-05 21:18:32 -0700

    fix problem statements

--- source/inject/modal.js
+++ source/inject/modal.js
@@ -1,37 +1,57 @@
 import { createElem, buildDomTree, addAttribs } from '../utils.js';
 
-function createModal(response) {
-    const appTag = "IJ"; 
-    const modal = createElem("div", {"class": `${apptag}modal`}, undefined);
-    
-    buildDomTree(modal.htmlElement, [
-        createElem("span", {"class": `${apptag}reading`}, undefined),
-        createElem("span", {"class": `${apptag}word`}, undefined),
-        createElem("span", {"class": `${apptag}definition`}, undefined),
-        createElem("span", {"class": `${apptag}partsOfSpeech`}, undefined),
-        createElem("a", {"class": `${apptag}jishoLink`}, undefined)
-    ]);
+const clickPos = {
+    x: 0,
+    y: 0,
+};
 
-    removeModal(document.getElementsByClassName(modal.class)[0]);
+export function setClickPos(event) {
+    clickPos.x = event.pageX;
+    clickPos.y = event.pageY;
+}
+
+export function* getAppTag() {
+    for (;;) {
+        yield "IJ";
+    } 
+}
+
+export function createModal(response) {
+    const appTag = getAppTag().next().value; 
     
+    const modal = createElem("div", {"id": `${appTag}modal`}, undefined);
+    const content = [
+        createElem("span", {"id": `${appTag}reading`}, undefined),
+        createElem("span", {"id": `${appTag}word`}, undefined),
+        createElem("span", {"id": `${appTag}definition`}, undefined),
+        createElem("span", {"id": `${appTag}partsOfSpeech`}, undefined),
+        createElem("a", {"id": `${appTag}jishoLink`}, undefined)
+    ];
+
+    content.forEach(elem => elem.setAttribute("class", `${appTag}content`));
+
+    buildDomTree(modal, content);
+
     // TODO find a way to get positioning from click without using
     // global variable
-    modal.style.left = position.x;
-    modal.style.top = position.y;
+    modal.style.left = `${(clickPos.x + 20).toString()}px`;
+    modal.style.top = `${(clickPos.y - 20).toString()}px`;
 
     document.body.appendChild(modal);
     document.addEventListener("click", function() {
         if (!isChild(modal, event.target)) {
-            modal.remove();
+            removeModal(modal);
             document.removeEventListener("click", this);
         }
     });
 
-    generateContent(modal, response);
+    generateContent(content, response);
 }
 
-function generateContent(modal, content) {
+function generateContent(content, response) {
     const toggle = 'T'.charCodeAt();
+    const appTag = getAppTag().next().value;
+    
     let index = 0;
 
     const changeContent = (event) => {
@@ -40,7 +60,7 @@ function generateContent(modal, content) {
         }
 
         if (response.status !== undefined) {
-            content.reading.definition = response.status;
+            content[2].innerHTML = response.status;
             return; 
         }
 
@@ -48,20 +68,15 @@ function generateContent(modal, content) {
         const japanese = result.japanese[0];
         const english = result.senses[0];
 
-        addAttribs([modal.reading, 
-                    modal.word, 
-                    modal.definitions, 
-                    modal.partsOfSpeech,
-                    modal.jishoLink], 
-                        [{"innerHTML": japanese.reading}, 
-                         {"innerHTML": japanese.word},
-                         {"innerHTML": english.english_definitions.join(', ')},
-                         {"innerHTML": english.parts_of_speech.join(', ')},
-                         {"innerHTML": "jisho.org", 
-                            "href": `http://jisho.org/search/${encodeURIComponent(japanese.word)}`, 
-                            "target": '_blank'}])
-
-        index++; 
+        addAttribs(content, [{"innerHTML": japanese.reading}, 
+                               {"innerHTML": japanese.word},
+                               {"innerHTML": english.english_definitions.join(', ')},
+                               {"innerHTML": english.parts_of_speech.join(', ')},
+                               {"innerHTML": "jisho.org", 
+                                "href": `http://jisho.org/search/${encodeURIComponent(japanese.word)}`, 
+                                "target": '_blank'}]);
+                          
+        index += 1; 
 
         if (index === response.data.length) {
             index = 0;
@@ -69,7 +84,7 @@ function generateContent(modal, content) {
     }
 
     changeContent();
-    modal.addEventListener('mouseup', changeContent);
+    document.getElementById(`${appTag}modal`).addEventListener('mouseup', changeContent);
     document.addEventListener('keyup', changeContent);
 }
 
@@ -78,7 +93,7 @@ function isChild(parentNode, node) {
         return false;
     }
 
-    if (node === ParentNode) {
+    if (node === parentNode) {
         return true;
     }
 
--- source/inject/utils.js
+++ source/inject/utils.js
@@ -1,6 +1,6 @@
 export function createElem(type, attribs, htmlContent) {
     if (!(typeof type === 'string')) {
-        console.warn(`createElem: Invalid first argument,
+        console.warn(`createElem: Invalid first argument, \
                       must be of type string`);
     }
 
@@ -32,8 +32,8 @@ export function buildDomTree(parent, children) {
             } 
         } else {
             if (!child.toString) {
-                console.warn(`buildDomTree: Invalid type in arguments,
-                              Node must be instance of Node or have a toString method.
+                console.warn(`buildDomTree: Invalid type in arguments, \
+                              Node must be instance of Node or have a toString method. \
                               Error found at ${child} of ${parent}`)
             }
             parent.appendChild(child instanceof Node? child : 
@@ -47,29 +47,37 @@ export function buildDomTree(parent, children) {
 export function addAttribs(element, attribs) {
     if (element instanceof Array) {
         if (!(attribs instanceof Array)) {
-            console.warn(`addAttribs: If element is given as an Array,
+            console.warn(`addAttribs: If element is given as an Array, \
                           attribs must also be an Array`);
             return;
         }
         if (element.length !== attribs.length) {
-            console.warn(`addAttribs: If element is given as an Array,
+            console.warn(`addAttribs: If element is given as an Array, \
                           attribs must have the same length as element`);
             return;
         }
 
-        for (const i in attribs) {
+        for (const i in element) {
             addAttribs(element[i], attribs[i]);
         }
+        
+        return;
     }
 
     if (!(element instanceof Element)) {
         console.warn(`addAttribs: An element must be instance of Element`);
     }
-
-    for (const attrib of attribs) {
+    
+    Object.keys(attribs).forEach((attrib) => {
         if (attrib === "innerHTML") {
-            element.appendChild(createTextNode(attribs[attrib]));
+            if (attribs[attrib] === undefined) {
+                element.innerHTML = '';
+                return;
+            }
+
+            element.innerHTML = attribs[attrib];
         }
-        element[attrib] = attribs[attrib];
-    }
+
+        element.setAttribute(attrib, attribs[attrib]);
+    });
 }
