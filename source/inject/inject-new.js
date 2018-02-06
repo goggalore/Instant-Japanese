ad73498a875a3398a810a3c9d467660cfc79a411
goggalore <junec177@gmail.com>
2018-02-05 21:19:52 -0700

    add new preliminary file for ES6 overhaul. Features the main inject code. Not yet implemented to run in app.

---
+++ source/inject/inject-new.js
@@ -0,0 +1,26 @@
+import { getAppTag, createModal, getClickPos, getAppTag } from './modal.js';
+
+document.addEventListener('mousedown', setClickPosition);
+document.addEventListener('keyup', onKeyPress);
+
+function onKeyPress(event) {
+    const selection = document.getSelection().toString().trim();
+    const toggle = 'T'.charCodeAt();
+    const maxLength = 20;
+
+    const appTag = getAppTag.next().value; 
+    const modal = document.getElementById(`${appTag}modal`);
+
+    if (selection && event.keycode === toggle && selection.length < maxLength && !modal) {
+        displayLoading();
+        chrome.runtime.sendMessage({searchTerm: selection}, createModal);
+    }
+}
+
+function displayLoading() { 
+    const loading = {
+        'status': 'Loading...'
+    };
+
+    createModal(loading);
+}
