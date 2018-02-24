b574b1ef4245485c45c86d675fc4d5cbeae6fd38
June <junec177@gmail.com>
2018-02-23 20:38:43 -0700

    rename inject.js to main.js

--- src/inject/main.js
+++ src/inject/main.js
@@ -1,17 +1,18 @@
-import { getAppTag, createModal, getClickPos } from './modal.js';
+import { getTextPosition, createModal, generateContent, isChild, removeModal} from './modal.js';
+import { buildDomTree, createElem, addAttribs } from '../util.js'
 
-document.addEventListener('mousedown', setClickPosition);
-document.addEventListener('keyup', onKeyPress);
+const appTag = "IJ";
+
+document.addEventListener("keyup", onKeyPress);
 
 function onKeyPress(event) {
     const selection = document.getSelection().toString().trim();
     const toggle = 'T'.charCodeAt();
     const maxLength = 20;
-
-    const appTag = getAppTag.next().value; 
+    
     const modal = document.getElementById(`${appTag}modal`);
 
-    if (selection && event.keycode === toggle && selection.length < maxLength && !modal) {
+    if (selection && event.keyCode === toggle && selection.length < maxLength && !modal) {
         displayLoading();
         chrome.runtime.sendMessage({searchTerm: selection}, createModal);
     }
@@ -19,7 +20,7 @@ function onKeyPress(event) {
 
 function displayLoading() { 
     const loading = {
-        'status': 'Loading...'
+        "status": "Loading..."
     };
 
     createModal(loading);
