1303129c01634da14daf9cfecac27b57be126495
June <junec177@gmail.com>
2018-02-23 20:24:00 -0700

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const rollup = require('rollup-stream');
+const source = require('vinyl-source-stream');
+
+gulp.task('rollup', () => {
+    return rollup({
+        input: './src/inject/main.js',
+        format: 'iife',
+    })
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['rollup'], () => {
+    gulp.watch(['./src/inject/main.js', './src/inject/modal.js', './src/util.js',
+                './src/event/event.js']);
+});
+
